32daea43bcea0582f167c4d0b4b2d4198d698360
zayaz <zayaz20022001@gmail.com>
2020-12-08 20:33:45 +0200

    DataBase material

--- server/dormitories/data.go
+++ server/dormitories/data.go
@@ -3,6 +3,9 @@ package dormitories
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+     "log"
+     "strconv"
 )
 
 type Student struct {
@@ -23,11 +26,89 @@ func NewDormitories(db *sql.DB) *Store {
 }
 
 func (s *Store) ListDormitories() ([]*Dormitory, error) {
-	res := []*Dormitory{}
-	return res, nil
+	querySelect := `SELECT d.id, s.specialty, COUNT(s.specialty)
+        FROM students s
+        JOIN dormitories d ON d.id = s.dormitory_id
+        GROUP BY d.id, s.specialty`
+        rows, err := s.Db.Query(querySelect)
+        if err != nil {
+            return nil, err
+        }
+
+        defer rows.Close()
+
+        var res []*Dormitory
+        var dorm *Dormitory
+        students := make(map[string]string)
+        var spec string
+        var count int64
+        for rows.Next() {
+            var curId int64
+            if err := rows.Scan(&curId, &spec, &count); err != nil {
+                return nil, err
+            }
+
+            if dorm == nil {
+                dorm = new(Dormitory)
+                dorm.Id = curId
+            } else {
+                if dorm.Id != curId {
+                    s0, _ := json.Marshal(students)
+                    s1 := json.RawMessage(s0)
+                    dorm.StudentsSpecialtiesQ = &s1
+                    res = append(res, dorm)
+
+                    dorm = new(Dormitory)
+                    dorm.Id = curId
+                    students = make(map[string]string)
+                }
+            }
+
+            students[spec] = strconv.FormatInt(count, 10)
+        }
+        if dorm != nil {
+            s0, _ := json.Marshal(students)
+            s1 := json.RawMessage(s0)
+            dorm.StudentsSpecialtiesQ = &s1
+            res = append(res, dorm)
+        }
+        if res == nil {
+            res = make([]*Dormitory, 0)
+        }
+        return res, nil
 }
 
 func (s *Store) AddStudent(specialty string) error {
+	if len(specialty) < 0 {
+            return fmt.Errorf("specialty is not provided")
+        }
 
-	return err
+        querySelect := `SELECT d.id
+        FROM students s
+        JOIN dormitories d ON d.id = s.dormitory_id
+        WHERE specialty = $1
+        GROUP BY d.id, s.specialty
+        ORDER BY COUNT(s.specialty)
+        LIMIT 1`
+        rows, err := s.Db.Query(querySelect, specialty)
+        if err != nil {
+            return err
+        }
+        defer rows.Close()
+
+        if !rows.Next() {
+            log.Println("aaaa")
+            return err
+        }
+
+        var id int64
+        if err := rows.Scan(&id); err != nil {
+            return err
+        }
+        log.Println(id)
+
+        queryInsert := `INSERT INTO students (dormitory_id, specialty)
+        VALUES ($1, $2)`
+        _, err = s.Db.Exec(queryInsert, id, specialty)
+        return err
 }
